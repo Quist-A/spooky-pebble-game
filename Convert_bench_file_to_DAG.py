4bee7cd5d572b4bac093055205b57b1f01285122
Arend-Jan Quist <66957583+Quist-A@users.noreply.github.com>
2023-02-17 17:21:37 +0100

    Add option to convert bench file to DAG in dag-class

--- Convert_bench_file_to_DAG.py
+++ Convert_bench_file_to_DAG.py
@@ -1,5 +1,109 @@
 import numpy as np
 
+
+
+class dag:
+    def __init__(self,filename=None):
+        self.n = 0  # number of vertices
+        self.output_vertices = []
+        self.edges = []
+	if filename != None:  
+		self.filename = filename
+		self.benchmarkname = filename.split("/")[-1][:-6]
+        	self.importBench(filename)
+        
+    def importBench(self,filename):
+        """
+        Convert circuit in .bench file to a directed acyclic graph (DAG) representation.
+	The inputs of the .bench file are not included in the grpah.
+        
+        Input:
+        filename (str): filename of .bench file to be converted
+                
+        Output:
+        n (int): number of vertices of DAG
+        output_vertices (numpy array): integer labels of output vertices of DAG
+        edges (numpy array): pairs of integer labels of edges of DAG
+        """
+
+        # open file
+        with open(filename, "r") as file:
+            data = file.read().splitlines()
+            
+        linenr = 0
+
+        # read lines with inputs
+        input_vertices = []
+        while (data[linenr][:5] == "INPUT"):
+            input_vertex = data[linenr][7:-1]
+            input_vertices.append(input_vertex)
+            linenr += 1
+        Ninputs = len(input_vertices)
+        
+        # read lines with outputs
+        while (data[linenr][:6] == "OUTPUT"):
+            # don't do anything...
+            linenr += 1   
+
+        # forget about the n0 = gnd line
+        linenr += 1 
+
+        # read lines with edges
+        while (data[linenr][0] == "n"):
+            split = data[linenr].split(" = ")
+
+            start_vertex = int(split[0][1:])-Ninputs-1  # gate
+
+            start = split[1].index("(")
+            end = split[1].index(")")
+            end_vertices = split[1][start+2:end].split(", n")  # inputs of gate
+
+            for end_vertex in end_vertices:
+                if (end_vertex != '0'):
+                    if (end_vertex not in input_vertices):
+                        self.edges.append((start_vertex,int(end_vertex)-Ninputs-1))
+            linenr += 1
+        
+        self.edges = np.array(self.edges)
+        self.n = np.max(self.edges)+1
+
+        # read lines with outputs
+        while (linenr < len(data) and data[linenr][:2] == "po"):
+            start = data[linenr].index("(")
+            end = data[linenr].index(")")
+            output_vertex = data[linenr][start+2:-1]
+            if (output_vertex not in input_vertices):
+                self.output_vertices.append(int(output_vertex)-Ninputs-1)
+            #else:
+            #    self.output_vertices.append(self.n)
+            #    self.n += 1
+            linenr += 1  
+
+        
+        self.output_vertices = np.array(self.output_vertices)
+        
+
+
+# EXAMPLE to import .bench file
+#
+#filename = "benchmarks/ISCAS85XMG/c7552.bench"
+#dag = dag(filename)
+
+
+
+
+
+
+
+
+
+
+#==========================================================================================
+#
+#                              SECOND OPTION TO IMPORT .BENCH FILES    (by function)
+#
+#==========================================================================================
+
 def benchToDAG(filename, rename_vertices = True, print_progress = False):
     """
     Convert circuit in .bench file to a directed acyclic graph (DAG) representation.
@@ -97,15 +201,15 @@ def benchToDAG(filename, rename_vertices = True, print_progress = False):
         # NOTE: dictionary is of type list to guarantee preserved order in for-loops
 
         # convert vertex numbers in edges
-	nr_of_edges = len(edges)
-	edges = np.array(edges).reshape(2*nr_of_edges)
-	for j,x in enumerate(dictionary):
-            edges[edges == x] = j
-	edges = np.array(edges, dtype=int).reshape([nr_of_edges,2]) # convert array type from strings to integers
+        nr_of_edges = len(edges)
+        edges = np.array(edges).reshape(2*nr_of_edges)
+        for j,x in enumerate(dictionary):
+                edges[edges == x] = j
+        edges = np.array(edges, dtype=int).reshape([nr_of_edges,2]) # convert array type from strings to integers
 
 
         # convert vertex numbers of input vertices
-	input_vertices = np.array(input_vertices)
+        input_vertices = np.array(input_vertices)
         for j,x in enumerate(dictionary):
             input_vertices[input_vertices == x] = j
         input_vertices = np.array(input_vertices, dtype=int) # convert array type from strings to integers

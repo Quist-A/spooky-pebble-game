338d8c990d54e7f18a500173b085ac8314cd6c51
A. Quist <quista1@u0027594.vuw.leidenuniv.nl>
2023-11-28 14:51:01 +0100

    Add plotter for optimized solutions

--- plotResults.py
+++ plotResults.py
@@ -5,57 +5,158 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as clrs
 
 
-def import_data(filename):
-
-	#f = open("final_results/"+benchmarkname+"_results_new_Tmax=8m_Twait=60s", "r")
-	#f = open("final_results/"+benchmarkname+"_results_new_Tmax=2m_Twait=15s", "r")
+def import_data(benchmarkname):
+	N_rnd_opt = 3  # number of random optimizers per SAT
+	
+	#=============
+	# IMPORT DATA
+	#=============
+	
+	filename = "final_results/"+benchmarkname+"_results"
 	f = open(filename, "r")
-
 	data = f.read()
-
 	f.close()
-
+	
 	inf = 123456
 
 	data = data.replace('(',"")
 	data = data.replace(')',"")
-	data = data.replace('\n',"")
+	data = data.replace('\n',",")
+	data = data.replace("SAT ","SAT,")
+	data = data.replace("OPT1 ","OPT1,")
+	data = data.replace("OPT2 ","OPT2,")
+	data = data.replace("OPT_RND ","OPT_RND,")
+	data = data.replace("OPTIMIZER_1_RUNTIME ","OPTIMIZER_1_RUNTIME,")
+	data = data.replace("OPTIMIZER_2_RUNTIME ","OPTIMIZER_2_RUNTIME,")
+	data = data.replace("OPTIMIZER_RND_RUNTIME ","OPTIMIZER_RND_RUNTIME,")
 	data = data.replace('inf',str(inf))
 	data = data.split(',')
-
-	solutionsstr = np.array(data[:-1]).reshape(int(len(data)/11),11)
-	solutions = np.zeros((int(len(data)/11),11))
-	solutions[:,:-1] = solutionsstr[:,:-1].astype(int)
-	solutions[:,-1] = solutionsstr[:,-1].astype(float)
-
-
-	solutions[solutions == inf] = solutions[0][0]  # replace inf by number of vertices
-
-
-	return solutions
-
-
-def convertSolutions(solutions):
-	seqTime = []
-	pebbles = []
-	spooks = []
-
-
-	runtimes = []
-	pebble_constr = []
-	spook_constr = []
-
-
-	for max_pebbles,max_spooks,pebbles_used,spooks_used,count,seqT,opt_pebbles_used,opt_spooks_used,opt_count,opt_seqT,runtime in solutions:
-		seqTime.append(opt_seqT)
-		pebbles.append(opt_pebbles_used)
-		spooks.append(opt_spooks_used)
-		
-		runtimes.append(runtime)
-		pebble_constr.append(max_pebbles)
-		spook_constr.append(max_spooks)
-	return seqTime, pebbles, spooks, runtimes, pebble_constr, spook_constr
 	
+	# remove empty elements
+	data = np.array(data)
+	index = np.argwhere(data=="")
+	data = np.delete(data, index)
+	
+	# TODO: replace inf by number of vertices
+	
+	
+	
+	#============
+	# PARSE DATA
+	#============
+	
+	seqTime, parTime, pebbles, spooks, runtimes, pebble_constr, spook_constr = [],[],[],[],[],[],[]
+	OPT1, OPT2, OPT_RND = [],[],[]
+	OPT1_runtimes, OPT2_runtimes, OPT_RND_runtimes = [],[],[]
+	
+	i=0
+	while(i<len(data)):
+		if (data[i] == "SAT"):
+			i+=1
+			pebbles.append(int(data[i]))
+			i+=1
+			spooks.append(int(data[i]))
+			i+=1
+			parTime.append(int(data[i]))
+			i+=1
+			pebble_constr.append(int(data[i]))
+			i+=1
+			spook_constr.append(int(data[i]))
+			i+=1
+			runtimes.append(float(data[i]))
+			seqTime.append(int(data[i+2]))
+			i+=1
+			tmp = []
+			while(data[i]=="OPT1"):
+				i+=1
+				tmp.append([int(data[i]),int(data[i+1]),int(data[i+2])])
+				i+=3
+			OPT1.append(tmp)
+			if(data[i]=="OPTIMIZER_1_RUNTIME"):
+				i+=1
+				OPT1_runtimes.append(float(data[i]))
+				i+=1
+			else:
+				print("[pareto plot] could not find optimizer runtime")
+			
+			tmp = []
+			while(data[i]=="OPT2"):
+				i+=1
+				tmp.append([int(data[i]),int(data[i+1]),int(data[i+2])])
+				i+=3
+			OPT2.append(tmp)
+			if(data[i]=="OPTIMIZER_2_RUNTIME"):
+				i+=1
+				OPT2_runtimes.append(float(data[i]))
+				i+=1
+			else:
+				print("[pareto plot] could not find optimizer runtime")
+			
+			for rnd_opt in range(N_rnd_opt):
+				tmp = []
+				while(data[i]=="OPT_RND"):
+					i+=1
+					tmp.append([int(data[i]),int(data[i+1]),int(data[i+2])])
+					i+=3
+				OPT_RND.append(tmp)
+				if(data[i]=="OPTIMIZER_RND_RUNTIME"):
+					i+=1
+					OPT_RND_runtimes.append(float(data[i]))
+					i+=1
+				else:
+					print("[pareto plot] could not find optimizer runtime")
+		else:
+			print("[pareto plot] could not parse data")	
+			i+=1
+	return seqTime, parTime, pebbles, spooks, runtimes, pebble_constr, spook_constr, OPT1, OPT2, OPT_RND, OPT1_runtimes, OPT2_runtimes, OPT_RND_runtimes
+
+
+
+
+def plotParetoFront(benchmarkname,ax=None):
+	seqTime, parTime, pebbles, spooks, runtimes, pebble_constr, spook_constr, OPT1, OPT2, OPT_RND, OPT1_runtimes, OPT2_runtimes, OPT_RND_runtimes = import_data(benchmarkname)
+	
+	if ax==None:
+		fig = plt.figure()
+		ax = fig.add_subplot(1,1,1)
+		
+	ax.set_ylabel('sequential time')
+	ax.set_xlabel('pebbles')
+
+
+	
+	
+	min_spooks = min(spooks)
+	max_spooks = max(spooks)
+
+	
+	
+	for path in OPT1+OPT2+OPT_RND:
+		xdata, ydata, cdata = [],[],[]
+		for [a,b,c] in path:
+			xdata.append(b)
+			ydata.append(a)
+			cdata.append(c)
+		#ax.plot(xdata,ydata,color="grey",linewidth=0.5,markersize=1,marker=None,alpha=0.1)
+		ax.scatter(xdata, ydata, c = cdata, cmap='viridis',s=1,alpha=0.15,vmin=min_spooks,vmax=max_spooks)
+	print(benchmarkname)
+	print("Minimal sequential time",min(xdata))
+	print("Minimal number of pebbles",min(ydata))
+	print("Maximal runtime solver",max(runtimes))
+	print("Maximal runtime optimizer",max(OPT1_runtimes+OPT2_runtimes+OPT_RND_runtimes))
+	
+	xdata = pebbles
+	ydata = seqTime
+	
+	scatterplot = ax.scatter(xdata, ydata, c = spooks, cmap='viridis',s=10,vmin=min_spooks,vmax=max_spooks)
+	
+	ax.set_title(benchmarkname)
+
+	plt.colorbar(scatterplot, label='spooks')
+	
+	#plt.show()
+	return
+
 
 def plotParetoFronts(benchmarknames):
 	fig = plt.figure(figsize=(8,12))
@@ -65,163 +166,28 @@ def plotParetoFronts(benchmarknames):
 
 
 	for i, benchmarkname in enumerate(benchmarknames):
-		try:
-			solutions = import_data("final_results/"+benchmarkname+"_results_new_Tmax=2m_Twait=15s")
-		except:
-			solutions = import_data("final_results/"+benchmarkname+"_results_new_Tmax=8m_Twait=60s")
-		seqTime, pebbles, spooks, runtimes, pebble_constr, spook_constr = convertSolutions(solutions)
-		
+		#print(i)
 		ax = fig.add_subplot(nrows,ncols, i+1)
-		ax.set_ylabel('sequential time')
-		ax.set_xlabel('pebbles')
-
-
-		xdata = pebbles
-		ydata = seqTime
-
-		scatterplot = ax.scatter(xdata, ydata, c = spooks, cmap='viridis',s=10)
-
-		ax.set_title(benchmarkname)
-
-		plt.colorbar(scatterplot, label='spooks')
-		
+		plotParetoFront(benchmarkname,ax)
 		
 	fig.tight_layout()
-	plt.savefig("final_results/images/ParetoFrontsBenchmarks.pdf")
+	#plt.savefig("final_results/images/ParetoFrontsBenchmarksOptimizers_without_paths.pdf")
 	#plt.show()
 	plt.close()
 	return
 
+#plotParetoFront("c432")
 
-def plotParetoFrontsLargeBM(benchmarknames):
-	fig = plt.figure(figsize=(8,5))
-	nrows=2
-	ncols=2
-	#fig2 = plt.figure(figsize=(10,15))
-
-
-	for i, benchmarkname in enumerate(benchmarknames):
-		try:
-			solutions = import_data("final_results/Appendix_results/"+benchmarkname+"_results_new_Tmax=2m_Twait=15s")
-		except:
-			solutions = import_data("final_results/Appendix_results/"+benchmarkname+"_results_new_Tmax=8m_Twait=60s")
-		seqTime, pebbles, spooks, runtimes, pebble_constr, spook_constr = convertSolutions(solutions)
-		
-		ax = fig.add_subplot(nrows,ncols, i+1)
-		ax.set_ylabel('sequential time')
-		ax.set_xlabel('pebbles')
-
-
-		xdata = pebbles
-		ydata = seqTime
-
-		scatterplot = ax.scatter(xdata, ydata, c = spooks, cmap='viridis',s=10)
-
-		ax.set_title(benchmarkname)
-
-		plt.colorbar(scatterplot, label='spooks')
-		
-		
-	fig.tight_layout()
-	plt.savefig("final_results/images/ParetoFrontsBenchmarksAppendix.pdf")
-	#plt.show()
-	plt.close()
-	return
-
-def plotConstraintRuntimes(benchmarknames):
-	fig = plt.figure(figsize=(8,12))
-	nrows=5
-	ncols=2
-
-	for i, benchmarkname in enumerate(benchmarknames):
-		try:
-			solutions = import_data("final_results/"+benchmarkname+"_results_new_Tmax=2m_Twait=15s")
-		except:
-			solutions = import_data("final_results/"+benchmarkname+"_results_new_Tmax=8m_Twait=60s")
-		seqTime, pebbles, spooks, runtimes, pebble_constr, spook_constr = convertSolutions(solutions)
-		
-		ax = fig.add_subplot(nrows,ncols, i+1)
-		ax.set_ylabel('runtime (seconds)')
-		ax.set_xlabel('pebbles constraint')
-
-
-		xdata = pebble_constr
-		ydata = runtimes
-		zdata = spook_constr
-
-		xdata = np.array(xdata)
-
-		scatterplot = ax.scatter(xdata, ydata, c = zdata, cmap='viridis', s = 10, norm=clrs.PowerNorm(gamma=1./3.5))
-		#label = z)#,label="x")
-		#norm=clrs.BoundaryNorm([0,1,int(max(spook_constr)/20),int(max(spook_constr)/10),int(max(spook_constr)/5), max(spook_constr)],256),#norm=clrs.LogNorm())# norm=clrs.PowerNorm(gamma=1./3.5))
-		if benchmarkname == "c7552": xdata[xdata == 944] = 988  # fix due to small data error
-		ax.set_xlim([min(xdata)-1,max(xdata[xdata<max(xdata)])+1])
-
-		ax.set_title(benchmarkname)
-
-		plt.colorbar(scatterplot, label='spooks constraint')
-		#ax.legend()
-		
-		
-
-	fig.tight_layout()
-	plt.savefig("final_results/images/ConstraintRuntimesBenchmarks.pdf")
-	#plt.show()
-	plt.close()
-	return
-
-
-def plotConstraintRuntimesLargeBM(benchmarknames):
-	fig = plt.figure(figsize=(8,5))
-	nrows=2
-	ncols=2
-
-	for i, benchmarkname in enumerate(benchmarknames):
-		try:
-			solutions = import_data("final_results/Appendix_results/"+benchmarkname+"_results_new_Tmax=2m_Twait=15s")
-		except:
-			solutions = import_data("final_results/Appendix_results/"+benchmarkname+"_results_new_Tmax=8m_Twait=60s")
-		seqTime, pebbles, spooks, runtimes, pebble_constr, spook_constr = convertSolutions(solutions)
-		
-		ax = fig.add_subplot(nrows,ncols, i+1)
-		ax.set_ylabel('runtime (seconds)')
-		ax.set_xlabel('pebbles constraint')
-
-
-		xdata = pebble_constr
-		ydata = runtimes
-		zdata = spook_constr
-
-		xdata = np.array(xdata)
-
-		scatterplot = ax.scatter(xdata, ydata, c = zdata, cmap='viridis', s = 10, norm=clrs.PowerNorm(gamma=1./3.5))
-		#label = z)#,label="x")
-		#norm=clrs.BoundaryNorm([0,1,int(max(spook_constr)/20),int(max(spook_constr)/10),int(max(spook_constr)/5), max(spook_constr)],256),#norm=clrs.LogNorm())# norm=clrs.PowerNorm(gamma=1./3.5))
-		if benchmarkname == "c3540": xdata[xdata == 806] = 830  # fix due to small data error
-		ax.set_xlim([min(xdata)-1,max(xdata[xdata<max(xdata)])+1])
-
-		ax.set_title(benchmarkname)
-
-		plt.colorbar(scatterplot, label='spooks constraint')
-		#ax.legend()
-		
-		
-
-	fig.tight_layout()
-	plt.savefig("final_results/images/ConstraintRuntimesBenchmarksAppendix.pdf")
-	#plt.show()
-	plt.close()
-	return
 
 # plot benchmarks results
 benchmarknames = ["c432","c499","c880","c1355","c1908","c2670","c3540","c5315","c6288","c7552"]
 plotParetoFronts(benchmarknames)
-plotConstraintRuntimes(benchmarknames)
+#plotConstraintRuntimes(benchmarknames)
 
 
 # plot large benchmarks results
-benchmarknames = ["c3540","c5315","c6288","c7552"]
-plotParetoFrontsLargeBM(benchmarknames)
-plotConstraintRuntimesLargeBM(benchmarknames)
+#benchmarknames = ["c3540","c5315","c6288","c7552"]
+#plotParetoFrontsLargeBM(benchmarknames)
+#plotConstraintRuntimesLargeBM(benchmarknames)
 
 
